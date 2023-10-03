c08b8548d4a9e7fd873d347a842f8839efeb9fbc
Kakezu <kaius.vanajas@student.laurea.fi>
2023-10-03 14:26:29 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This website is inspired by Twitter. It has some same functionalities such as:
+Adding a tweet
+liking and retweeting other tweets
+
+This is made with a tutorial from Scrimba with some added functionalities with localStorage and a delete button.
---
+++ index.js
@@ -0,0 +1,204 @@
+import { tweetsData as mutableTweetsData } from './data.js'
+
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+let tweetsData = mutableTweetsData;
+
+tweetsData = JSON.parse(localStorage.getItem("tweetsData")) || tweetsData;
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
+    else if(e.target.id === 'tweet-btn'){
+        handleTweetBtnClick()
+    }
+    else if (e.target.dataset.delete) {
+        if (e.target.dataset.delete) {
+            handleDeleteClick(e.target.dataset.delete)
+        }
+    }
+})
+ 
+function handleLikeClick(tweetId){ 
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }
+    else{
+        targetTweetObj.likes++ 
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+    localStorage.setItem("tweetsData", JSON.stringify(tweetsData)); // Setting up localStorage for likes
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render() 
+    localStorage.setItem("tweetsData", JSON.stringify(tweetsData)); // Setting up localStorage for retweets
+}
+
+function handleReplyClick(replyId){
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+}
+
+function handleTweetBtnClick(){
+    const tweetInput = document.getElementById('tweet-input')
+
+    if(tweetInput.value){
+        tweetsData.unshift({
+            handle: `@Kakezu`,
+            profilePic: `images/omakuva.jpg`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4()
+        })
+    render()
+    tweetInput.value = ''
+    localStorage.setItem("tweetsData", JSON.stringify(tweetsData)); // Setting up localStorage for newer Tweets
+      
+    }
+
+}
+
+function handleDeleteClick(tweetId) {
+    // Find the tweet with the given ID
+    const targetTweetObj = tweetsData.find(function(tweet){
+        return tweet.uuid === tweetId;
+    });
+
+    // Check if the tweet belongs to the current user
+    if (targetTweetObj.handle !== "@Kakezu") {
+        // The tweet does not belong to the current user, so do not delete it
+        return;
+    }
+
+    // The tweet belongs to the current user, so delete it
+    tweetsData = tweetsData.filter(function(tweet){
+        return tweet.uuid !== tweetId;
+    });
+
+    // Update localStorage
+    localStorage.setItem("tweetsData", JSON.stringify(tweetsData));
+
+    // Re-render the tweets
+    render();
+}
+
+
+function getFeedHtml(){
+    let feedHtml = ``
+    
+    tweetsData.forEach(function(tweet){
+        
+        let likeIconClass = ''
+        
+        if (tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+        
+        let retweetIconClass = ''
+        
+        if (tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
+        
+        let repliesHtml = ''
+        
+        if(tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml+=`
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+`
+            })
+        }
+        
+        let deleteIconHtml = ''
+        
+        // Only add the trash icon if the tweet belongs to the current user
+        if (tweet.handle === "@Kakezu") {
+            deleteIconHtml = `
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-trash"
+                    data-delete="${tweet.uuid}"></i>
+                </span>
+            `
+        }
+          
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart ${likeIconClass}"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+                ${deleteIconHtml}
+            </div>   
+        </div>            
+    </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+        ${repliesHtml}
+    </div>   
+</div>
+`
+   })
+   return feedHtml 
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
+
